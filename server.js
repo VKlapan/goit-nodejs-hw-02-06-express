31c7435f5460751426634b9d333c290421c25790
VKlapan <vladimir.klapan@gmail.com>
2022-12-09 17:42:43 +0200

    fix starting server bags

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
 
-const PORT = 3001;
+const PORT = 3000;
 const uriDb =
   "mongodb+srv://klapan:2VpZA4uf6aHQVSrP@cluster0.bqrd9iv.mongodb.net/contactbook-db?retryWrites=true&w=majority";
 
@@ -17,9 +17,9 @@ const connection = mongoose.connect(uriDb, {
 connection
   .then(() => {
     app.listen(PORT, function () {
-      console.log(`DB Server running. Use our API on port: ${PORT}`);
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((err) =>
-    console.log(`DB Server not running. Error message: ${err.message}`)
+    console.log(`Server not running. Error message: ${err.message}`)
   );
