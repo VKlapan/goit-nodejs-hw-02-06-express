8a670089c8ae40eabaec4362bd479eb4706f4d85
VKlapan <vladimir.klapan@gmail.com>
2023-01-01 21:18:41 +0200

    add AWS SES

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -3,8 +3,9 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const config = {
-  host: "smtp.ethereal.email",
-  port: 587,
+  host: "email-smtp.eu-west-1.amazonaws.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
@@ -13,8 +14,8 @@ const config = {
 
 const transporter = nodemailer.createTransport(config);
 const emailOptions = {
-  from: "alberta.walter@ethereal.email",
-  to: "vladimir.klapan@gmail.com",
+  from: "v.lahodnyi@proformula.ua",
+  to: "v.klapan@proformula.ua",
   subject: "New mail from NODE.JS server",
   text: "It is a test. I try to check how nodemailer works...",
 };
